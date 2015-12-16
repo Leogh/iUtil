2cb713fa3573e5906889ce0e36ceeed2d1ebc26f
Leogh <leonardzheng@qq.com>
2015-12-16 18:57:13 +0800

    add delete folder function

---
+++ iUtil/iUtil/IO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iUtil
+{
+    public class IO
+    {
+        /// <summary>
+        /// 清空指定的文件夹，并删除文件夹
+        /// </summary>
+        /// <param name="dir"></param>
+        public static void DeleteFolder(string dir)
+        {
+            foreach (string d in Directory.GetFileSystemEntries(dir))
+            {
+                if (File.Exists(d))
+                {
+                    FileInfo fi = new FileInfo(d);
+                    if (fi.Attributes.ToString().IndexOf("ReadOnly") != -1)
+                        fi.Attributes = FileAttributes.Normal;
+                    File.Delete(d);//直接删除其中的文件  
+                }
+                else
+                {
+                    DirectoryInfo d1 = new DirectoryInfo(d);
+                    DeleteFolder(d1.FullName);////递归删除子文件夹
+                }
+            }
+            Directory.Delete(dir);
+        }
+    }
+}
